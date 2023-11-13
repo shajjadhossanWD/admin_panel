441bd4d9c8cf7a6cfd5ff66cb120a6b4ab8ccb15
shajjadhossanWD <shajjadhossan111@gmail.com>
2023-11-13 12:19:36 +0600

    push

--- src/index.js
+++ src/index.js
@@ -4,11 +4,9 @@ import "./assets/css/bootstrap.min.css";
 import "./assets/css/flaticon.min.css";
 import "./assets/css/boxicon.min.css";
 import "./index.css";
-import "./assets/css/responsive.min.css";
 import { Provider } from "react-redux";
 import store from "./redux/Store";
 import App from "./App";
-// import DslProvider from "./contexts/DSLCommerceContext";
 import AdminProvider from "./contexts/AdminContext";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
@@ -18,20 +16,13 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <AdminProvider>
-        {/* <DslProvider> */}
-            
-                <Provider store={store}>
-                  <App />
-                </Provider>
-              
-        {/* </DslProvider> */}
-      </AdminProvider>
+      <AdminProvider>            
+          <Provider store={store}>
+            <App />
+          </Provider>
+        </AdminProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
